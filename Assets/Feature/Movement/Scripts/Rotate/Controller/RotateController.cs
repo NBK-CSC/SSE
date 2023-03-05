e6bffc3e968cd46924e0ca21e4721e404028bc5d
NBK <1justforstuding1@gmail.com>
2023-03-05 17:32:44 +0300

    set delta mouse settings

--- Assets/Feature/Movement/Scripts/Rotate/Controller/RotateController.cs
+++ Assets/Feature/Movement/Scripts/Rotate/Controller/RotateController.cs
@@ -43,7 +43,7 @@ namespace SSE.Movement.Rotate.Controller
                 && CheckSwivelRange(_startRotation.y, newRotation.y)
                 && CheckSwivelRange(_startRotation.z, newRotation.z))
             {
-                _transform.Rotate(_rotation * speed);
+                _transform.Rotate(_rotation * speed * Time.deltaTime);
             }
         }
         
